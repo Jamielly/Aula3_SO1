e1c937491fd68389bb8a639d5d89887842471ddb
jami <jami@LAPTOP-N7LOO51M>
2022-09-07 19:58:40 -0300

    aula 3 de SO1

---
+++ ThreadVetor/src/view/Principal.java
@@ -0,0 +1,41 @@
+package view;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Lernum = new Scanner(System.in);
+		int Vet[] = new int[1000];
+	    int numUsuario;
+		
+		System.out.println("Digite um numero: ");
+		numUsuario = Lernum.nextInt();
+		
+		switch(numUsuario) {
+		case 0: 
+			numUsuario % 2 <= 0;
+			
+			for (int i = 0 ; i < Vet.length; i++) {
+				System.out.println("O numero digitado e par!");
+			while (!interrupted()) {
+				int numAleatorio = (int) (1 + Math.random() * 1000);
+				System.out.println("ThreadVet: " + numAleatorio);
+				break;
+			}
+			}
+			case 1: 
+				while (numUsuario % 2 >= 0){
+			System.out.println("O numero digitado e impar!!");
+				}
+		}
+	}
+			private void foreach(Class<Integer> class1) {
+				foreach(int Vet = new Vetor); {
+				Console.WriteLine(Vet);
+			}
+			Console.ReadLine();
+					}
+}
+}
